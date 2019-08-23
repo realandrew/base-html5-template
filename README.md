fdd1e732486531ea508b77d4c85be5c5f5430bfc
Andrew Mitchell <andrewjmitchell2000@gmail.com>
2019-08-23 02:35:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Info
+
+This is a basic template for HTML5-stack websites created by Andrew Mitchell.
+This template is licensed under LGPL-3.0.
---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+* Author: Andrew Mitchell
+* Creation date:
+* Last modified date:
+* Copyright © Andrew Mitchell _INSERT_YEAR_HERE_
+*/
+
+// Runs after documents and content finish loading
+window.onload = () => {
+
+}
