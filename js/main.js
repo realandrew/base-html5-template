42daf8ae6170add1382613ea7b90ed2edc95789b
Andrew Mitchell <andrewjmitchell2000@gmail.com>
2021-10-30 03:07:19 -0400

    The default JavaScript file now loads asynchronously by default. In 2021 this should be the standard for performance/latency of the end user. I also added a comment to inform the user how revert to synchronous load if needed.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Your standard javascript file. This is not for TypeScript or other compiled JavaScript alternatives. For help see https://developer.mozilla.org/en-US/docs/Web/JavaScript.
+// This script loads asynchronously. If you need to use it before it loads, you can remove 'async' from the script include tag in index.html.
 
 window.addEventListener('load', (event) => {
   // Runs after document and content finish loading
